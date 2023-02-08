01f4e53db34d122e248d210ebb9de37c543f2d1a
Gtpatel02 <gtpatel02@gmail.com>
2023-02-07 19:29:53 -0500

    Attempt connection to AWS and upload README

--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
 // imports
+const AWS = require('aws-sdk');
 const express = require('express');
 const cors = require('cors');
 const env = require('dotenv');
+const fs = require('fs');
 
 // load additional environment variables (ex: database and storage bucket passwords) from .env file
 env.config();
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_BUCKET_SECRET
+})
+
+const filename = 'README.md'
+const fileContent = fs.readFileSync(filename)
+
+const params = {
+  Bucket: process.env.AWS_BUCKET_NAME,
+  Key: `${filename}.jpg`,
+  Body: fileContent
+}
+
+s3.upload(params, (err, data) => {
+  if (err) {
+    console.error(err);
+    reject(err)
+  }
+  resolve(data.Location)
+})
 
 // create express app and configure it
 const app = express();

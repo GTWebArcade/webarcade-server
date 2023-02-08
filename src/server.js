c9cf3716786b5d579a9c46c86b2a4ccaf9fe5332
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-02-07 22:06:56 -0500

    Fix upload to s3 example

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 // imports
-const AWS = require('aws-sdk');
 const express = require('express');
 const cors = require('cors');
 const env = require('dotenv');
@@ -7,27 +8,30 @@ const fs = require('fs');
 
 // load additional environment variables (ex: database and storage bucket passwords) from .env file
 env.config();
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_BUCKET_SECRET
-})
 
-const filename = 'README.md'
-const fileContent = fs.readFileSync(filename)
-
-const params = {
-  Bucket: process.env.AWS_BUCKET_NAME,
-  Key: `${filename}.jpg`,
-  Body: fileContent
+// example of how to upload a file to s3
+{
+  const AWS = require('aws-sdk');
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_BUCKET_SECRET,
+  });
+  const filename = 'README.md';
+  const fileContent = fs.readFileSync(filename);
+  const awsFileSavePath = `web-arcade/${filename}`;
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: awsFileSavePath,
+    Body: fileContent,
+  };
+  // eslint-disable-next-line no-unused-vars
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error('Error uploading file', err);
+    }
+    console.log(`Uploaded file to ${process.env.AWS_CLOUDFRONT_URL}/${awsFileSavePath}`);
+  });
 }
-
-s3.upload(params, (err, data) => {
-  if (err) {
-    console.error(err);
-    reject(err)
-  }
-  resolve(data.Location)
-})
 
 // create express app and configure it
 const app = express();

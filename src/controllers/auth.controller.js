6c4e8ef28c863ece91714529e15650fbfac85481
Eunki-H <lship0425@gmail.com>
2023-02-20 14:48:34 -0500

    updated auth.controller.js & server.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,24 @@
-exports.signin = async (req, res) => {
-  res.status(500).send({ message: 'Endpoint not implemented.' });
+// Sign-up Endpoint
+
+// const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+
+// var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+exports.signup = (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8)
+  });
+
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.send({ message: "User was registered successfully!" });
+  });
 };

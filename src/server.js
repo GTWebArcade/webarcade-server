aeafad427d5cc0d90ab5b5af18e3bd5ec1ee8126
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-02-20 22:44:21 -0500

    Fix duplicate database variable which crashed build

--- src/server.js
+++ src/server.js
@@ -61,21 +61,6 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = require('./models');
-
-db.mongoose
-  .connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Successfully connected to MongoDB.');
-  })
-  .catch((err) => {
-    console.error('Could not connect to MongoDB.', err);
-    process.exit();
-  });
-
 // simple route
 app.get('/', (req, res) => {
   res.json({ message: 'WebArcade server.' });

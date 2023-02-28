872a85c46426ea2ebe03544ff5cdcd79be889f14
Varun Vudathu <varun.vudathu@gmail.com>
2023-02-27 19:33:36 -0500

    game endpoint implemented

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const controller = require('../controllers/auth.controller');
 const gameController = require('../controllers/game.controller');
+const { verifySignUp } = require('../middlewares');
 
 module.exports = function addAuthRoutes(app) {
   app.use((req, res, next) => {
@@ -11,6 +12,6 @@ module.exports = function addAuthRoutes(app) {
   });
 
   app.post('/api/v1/auth/sign-in', controller.signin);
-  app.post('/api/v1/auth/sign-up', controller.signup);
+  app.post('/api/v1/auth/sign-up', verifySignUp.checkDuplicateUsernameOrEmail, controller.signup);
   app.get('/api/v1/games', gameController.getGames);
 };

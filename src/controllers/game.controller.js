f6e9e3a59fd1835b570488cc6d6fa2e058319021
Varun Vudathu <varun.vudathu@gmail.com>
2023-03-14 18:52:12 -0400

    initial create game

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -12,3 +12,14 @@ exports.getGames = async (req, res) => {
     res.status(200).send({ games });
   });
 };
+
+exports.createGame = async (req, res) => {
+  Game.find({}).exec((err, games) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    res.status(200).send({ games });
+  });
+};

c0bfc70ae7b17bd51ec0b0b32f1bb2af6a1cf5de
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-04-11 18:54:29 -0400

    Fix style checker

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    name: String,
-    description: String,
-    gameType: String,
-    dataUrl: String,
-    loaderUrl: String,
-    frameworkUrl: String,
-    codeUrl: String,
-    imageUrl: String,
-    uploaderUserId: String,
-    createdAt: String,
-    updatedAt: String,
+  name: String,
+  description: String,
+  gameType: String,
+  dataUrl: String,
+  loaderUrl: String,
+  frameworkUrl: String,
+  codeUrl: String,
+  imageUrl: String,
+  uploaderUserId: String,
+  createdAt: String,
+  updatedAt: String,
 });
 schema.index({ name: 'text' });
 

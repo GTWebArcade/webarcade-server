16828d98d3cb011a884d23dc3713f580cc212b8d
Eunki Hong <71096881+Eunki-H@users.noreply.github.com>
2023-04-11 18:54:59 -0400

    22 allow more game filtering options for get games endpoint for server search date uploaded etc (#26)
    * latest updates
    
    * latest updates
    
    * add get/ game filtering by game name
    
    * Fix style checker
    
    ---------
    
    Co-authored-by: Deepak Ramalingam <rdeepak2002@gmail.com>

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -4,14 +4,32 @@ const db = require('../models');
 const Game = db.game;
 
 exports.getGames = async (req, res) => {
-  Game.find({}).exec((err, games) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
+  const nameQueryParam = req?.query?.name;
+  // const typeQeuryParam = req?.query?.type;
+  if (nameQueryParam) {
+    // mongoDB code
+    // Game.find({ name: nameQueryParam }).exec((err, games) => {
+    Game.find({ $text: { $search: nameQueryParam } })
+      // .skip(20)
+      // .limit(10)
+      .exec((err, games) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
 
-    res.status(200).send({ games });
-  });
+        res.status(200).send({ games });
+      });
+  } else {
+    Game.find({}).exec((err, games) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      res.status(200).send({ games });
+    });
+  }
 };
 
 exports.createGame = async (req, res) => {
--- src/models/game.model.js
+++ src/models/game.model.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-const Game = mongoose.model(
-  'Game',
-  new mongoose.Schema({
-    name: String,
-    description: String,
-    gameType: String,
-    dataUrl: String,
-    loaderUrl: String,
-    frameworkUrl: String,
-    codeUrl: String,
-    imageUrl: String,
-    uploaderUserId: String,
-    createdAt: String,
-    updatedAt: String,
-  }),
-);
+const schema = new mongoose.Schema({
+  name: String,
+  description: String,
+  gameType: String,
+  dataUrl: String,
+  loaderUrl: String,
+  frameworkUrl: String,
+  codeUrl: String,
+  imageUrl: String,
+  uploaderUserId: String,
+  createdAt: String,
+  updatedAt: String,
+});
+schema.index({ name: 'text' });
+
+const Game = mongoose.model('Game', schema);
 
 module.exports = Game;

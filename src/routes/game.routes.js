0e2a493ed72f75c8233e7fe11b28f5148e8eb3b6
Eunki-H <lship0425@gmail.com>
2023-04-11 19:26:42 -0400

    authentication verify token update

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,4 +1,5 @@
 const gameController = require('../controllers/game.controller');
+const authJwt = require('../middlewares/authJwt');
 
 module.exports = function addGameRoutes(app) {
   app.use((req, res, next) => {
@@ -9,7 +10,7 @@ module.exports = function addGameRoutes(app) {
     next();
   });
 
-  app.get('/api/v1/games', gameController.getGames);
-  app.get('/api/v1/game/:id', gameController.getGame);
-  app.post('/api/v1/game/create-game', gameController.createGame);
+  app.get('/api/v1/games', [authJwt.verifyToken], gameController.getGames);
+  app.get('/api/v1/game/:id', [authJwt.verifyToken], gameController.getGame);
+  app.post('/api/v1/game/create-game', [authJwt.verifyToken], gameController.createGame);
 };

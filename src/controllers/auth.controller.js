89f764ba4c6293bfd1244e8165def4dec0ef7c3a
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-04-15 16:27:00 -0400

    Increase auth token time to 2 weeks

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -35,7 +35,7 @@ exports.signin = async (req, res) => {
     }
 
     const token = jwt.sign({ id: user?.id }, config?.secret, {
-      expiresIn: 86400,
+      expiresIn: 14 * 86400,
     });
 
     res.status(200).send({
